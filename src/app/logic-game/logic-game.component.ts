6cd8f5e176b6b7ed98706b56ef10fd3dac014d79
Luis RoaH <luis.roa@avaldigitallabs.com>
2021-10-14 22:55:32 -0500

    update container
    update container

--- src/app/logic-game/logic-game.component.ts
+++ src/app/logic-game/logic-game.component.ts
@@ -7,12 +7,24 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./logic-game.component.scss'],
 })
 export class LogicGameComponent implements OnInit {
-  
+
+  constructor(public alertController: AlertController) { }
+  firstValue: number;
+  secondValue: number;
+  operator: string;
+  value: number;
+  total: number;
+  contador: number = 0;
+  n: number;
+  chars: string;
+  result: string;
+  totalOperator: number;
+
   async presentAlert() {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Operacion suma/resta*',
-      subHeader:`${this.firstValue}`+'-'+`${this.secondValue}`,
+      subHeader:`${this.firstValue}`+`${this.result}`+`${this.secondValue}`,
       message: `La respuesta correcta es ${this.total}`,
       buttons: ['OK']
     });
@@ -22,18 +34,6 @@ export class LogicGameComponent implements OnInit {
     const { role } = await alert.onDidDismiss();
     console.log('onDidDismiss resolved with role', role);
   }
-
-  constructor(public alertController: AlertController) { }
-  firstValue: number;
-  secondValue: number;
-  operator: string;
-  value: number;
-  total: number;
-  contador: number;
-  n: number;
-  chars: string;
-  result: string;
-  totalOperator: number;
 
   ngOnInit() {
     this.generateOperator();
@@ -59,16 +59,15 @@ export class LogicGameComponent implements OnInit {
     this.value = parseFloat((document.getElementById("inputTotal") as HTMLInputElement).value);
     this.resultOperator(this.firstValue,this.secondValue,this.result);
     this.total =this.totalOperator;
-    this.presentAlert();
-    console.log(this.result);
-    if (this.total===this.value) {
-      this.contador += 1;
+
+    if (this.total!=this.value) {
+      this.presentAlert();
     } 
     else{
-      this.contador -= 1;
+      this.contador += 1;
     }
     this.ngOnInit();
-    // document.getElementById("inputTotal").innerHTML="";
+    // document.getElementById("inputTotal").innerText="0";
   }
 
   resultOperator(first: number, second: number, mathOperator: string){   
