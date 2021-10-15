e186fe0286188fc636af2839b18f37da7f03f177
Luis RoaH <luis.roa@avaldigitallabs.com>
2021-10-14 21:55:18 -0500

    Add toast Operation
    Add toast Operation

--- src/app/logic-game/logic-game.component.ts
+++ src/app/logic-game/logic-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-logic-game',
@@ -6,14 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logic-game.component.scss'],
 })
 export class LogicGameComponent implements OnInit {
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Operacion suma/resta*',
+      subHeader: 'resultado',
+      message: `La respuesta correcta es ${this.total}`,
+      buttons: ['OK']
+    });
 
-  constructor() { }
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  constructor(public alertController: AlertController) { }
   firstValue: number;
   secondValue: number;
   operator: string;
+  value: number;
+  total: number;
+  contador: number;
+  n: number;
+  chars: string;
+  result: string;
+
 
   ngOnInit() {
     this.firstValue = Math.floor(Math.random()*101);
     this.secondValue = Math.floor(Math.random()*101);
+    this.result = '';
+    this.chars = '+-*/'
+   
+    this.n = this.chars.length;
+    for (let i = 0; i < this.n; i++){
+    this.result += this.chars.charAt(Math.floor(Math.random() * this.chars.length));
+    }
+    this.result=this.result.substring(0,1);
   }
+  
+  valite(){
+    this.value = parseFloat((document.getElementById("inputTotal") as HTMLInputElement).value);
+    this.total =this.firstValue - this.secondValue;
+    this.presentAlert();
+    console.log(this.result);
+    if (this.total==this.value) {
+      this.contador += 1;
+    } 
+    else{
+      this.contador -= 1;
+    }
+  }
+
 }
