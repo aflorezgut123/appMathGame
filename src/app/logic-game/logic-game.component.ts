d4d6d93af07a5dad2324cb21dc4a0abd3facd885
aflorezgut123 <andersonflorez9402@gmail.com>
2021-10-16 01:42:38 -0500

    ajustes finales
    ajustes finales

--- src/app/logic-game/logic-game.component.ts
+++ src/app/logic-game/logic-game.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { timer } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logic-game',
   templateUrl: './logic-game.component.html',
   styleUrls: ['./logic-game.component.scss'],
 })
-export class LogicGameComponent implements OnInit {
+export class LogicGameComponent implements OnInit { 
 
-  constructor(public alertController: AlertController) { }
+  constructor(public alertController: AlertController, private router: Router) { }
   firstValue: number;
   secondValue: number;
   operator: string;
@@ -19,25 +22,46 @@ export class LogicGameComponent implements OnInit {
   chars: string;
   result: string;
   totalOperator: number;
-
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Operacion suma/resta*',
-      subHeader:`${this.firstValue}`+`${this.result}`+`${this.secondValue}`,
-      message: `La respuesta correcta es ${this.total}`,
-      buttons: ['OK']
-    });
-
-    await alert.present();
-
-    const { role } = await alert.onDidDismiss();
-    console.log('onDidDismiss resolved with role', role);
-  }
+  timeLeft: number = 15;
+  interval;
 
   ngOnInit() {
     this.generateOperator();
     this.generateValues();    
+    this.startTimer();
+    this.resultOperator(this.firstValue,this.secondValue,this.result);
+    this.total =this.totalOperator;    
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Game Over',
+      subHeader:`Operation: ${this.firstValue}`+`${this.result}`+`${this.secondValue}`,
+      message: `Correct answer is: ${this.total}`,
+      buttons: ['OK']
+    });    
+    await alert.present();   
+    this.router.navigate(['/home']);
+    const { role } = await alert.onDidDismiss();    
+    this.pauseTimer();   
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      }else if(this.timeLeft < 1){
+        this.pauseTimer();
+        this.presentAlert();
+      } else {
+        this.timeLeft = 15;
+      }
+    },1000)
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
   }
 
   generateOperator(){
@@ -54,20 +78,21 @@ export class LogicGameComponent implements OnInit {
     this.firstValue = Math.floor(Math.random()*101);
     this.secondValue = Math.floor(Math.random()*101);
   }
-  
+
   valite(){
+    this.pauseTimer();
     this.value = parseFloat((document.getElementById("inputTotal") as HTMLInputElement).value);
     this.resultOperator(this.firstValue,this.secondValue,this.result);
     this.total =this.totalOperator;
 
     if (this.total!=this.value) {
-      this.presentAlert();
+      this.presentAlert();            
     } 
     else{
       this.contador += 1;
+      this.timeLeft = 15;      
     }
-    this.ngOnInit();
-    document.getElementById("inputTotal").focus();
+    this.ngOnInit();    
   }
 
   resultOperator(first: number, second: number, mathOperator: string){   
